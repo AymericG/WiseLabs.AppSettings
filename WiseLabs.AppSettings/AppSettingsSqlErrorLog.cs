c6359022ff9ee081880148a84e5058f53b87feba
Aymeric <aymeric.github@yopmail.com>
2015-09-04 15:01:16 +1000

    First commit

---
+++ WiseLabs.AppSettings/AppSettingsSqlErrorLog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Configuration;
+using Elmah;
+
+namespace WiseLabs.AppSettings
+{
+    public class AppSettingsSqlErrorLog : SqlErrorLog
+    {
+        protected string appSettingsKey;
+        public AppSettingsSqlErrorLog(IDictionary config)
+            : base(config)
+        {
+            appSettingsKey = (string)config["appSettingsKey"];
+        }
+
+        public override string ConnectionString
+        {
+            get { return ConfigurationManager.AppSettings[appSettingsKey]; }
+        }
+    }
+}
